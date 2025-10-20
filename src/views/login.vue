7251fa720bd56b9126b1d75228e51070200b4ef7
lnrelax <1021244230@qq.com>
2025-10-20 18:19:21 +0800

    修改配置

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="login">
     <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form">
-      <h3 class="title">哈希云码</h3>
+      <h3 class="title">全富生活</h3>
       <el-form-item prop="username">
         <el-input
           v-model="loginForm.username"
@@ -47,7 +47,7 @@
     </el-form>
     <!--  底部  -->
     <div class="el-login-footer">
-      <a href="https://beian.miit.gov.cn/" target="_blank">晋ICP备2025067387号-1</a>
+      <a href="https://beian.miit.gov.cn/" target="_blank">晋ICP备2025067734号-1</a>
     </div>
   </div>
 </template>
