747119aac6bcfc59e2eca8a9794d50659540edb7
lnrelax <1021244230@qq.com>
2026-01-12 17:14:15 +0800

    修改配置

--- src/views/image.vue
+++ src/views/image.vue
@@ -43,7 +43,7 @@
           <el-form-item label="图片" :label-width="formLabelWidth" >
             <el-upload
               class="avatar-uploader"
-              action="https://www.lxhkj.com/zxtest/image/imageUpload"
+              action="https://www.bjqmyjk.com/zxtest/image/imageUpload"
               :show-file-list="false"
               :on-success="imageSuccess"
               :before-upload="imageUpload">
